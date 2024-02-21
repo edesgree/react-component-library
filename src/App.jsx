c29392b3d1da5366b48535261522d36e78e8bfb1
Edouard Desgrée <edouard.desgree@gmail.com>
2024-02-21 18:36:04 +0100

    make badge css scoped

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import './App.css';
 import Badge from './components/Badge';
 
 function App() {
-    const colors = ['gray','red',  'yellow', 'green', 'blue','indigo','purple','pink']
+    const badgesColors = ['gray','red', 'yellow', 'green', 'blue','indigo','purple','pink']
     return (
         <>
             <h1>Your components go here</h1>
             <h2>Badges</h2>
-            {colors.map((color) => (
+            Default square : 
+            {badgesColors.map((color) => (
                 <Badge key={color} color={color}>
                     Badge {color}
                 </Badge>
             ))}
-             
+             Pill shape : 
+            {badgesColors.map((color) => (
+                <Badge key={color} color={color} pill>
+                    Badge {color}
+                </Badge>
+                ))}
         </>
     )
   }
--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,8 +1,10 @@
-import "./badge.css";
+import styles from "./Badge.module.css";
 
-export default function Badge({ children, color }) {
+export default function Badge({ children, color, pill }) {
+    console.log(styles);
+    const badgeCss = [styles.badge, styles[color], pill ? styles['badge-pill'] : ""].join(" ");
     return (
-        <span className={`badge ${color}`}>{children}</span>
-    )
+        <span className={badgeCss}>{children}</span>
+    );
 }
 
